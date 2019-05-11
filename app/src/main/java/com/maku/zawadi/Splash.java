2bc703860163ca33e7d8cae6126803f9653f1cdf
ma-za-kpe <makpalyy@gmail.com>
2019-05-11 11:47:59 +0200

    added the splash screen activity and changes the primary colors to pink

---
+++ app/src/main/java/com/maku/zawadi/Splash.java
@@ -0,0 +1,21 @@
+package com.maku.zawadi;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        // Start home activity
+        startActivity(new Intent(Splash.this, MainActivity.class));
+        // close splash activity
+        finish();
+
+    }
+}
