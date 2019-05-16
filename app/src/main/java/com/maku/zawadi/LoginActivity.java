3e7673747e7cb3eb86ede25ea09c5ad7075901fc
ma-za-kpe <makpalyy@gmail.com>
2019-05-16 19:55:34 +0200

    added google sign in to the app

--- app/src/main/java/com/maku/zawadi/LoginActivity.java
+++ app/src/main/java/com/maku/zawadi/LoginActivity.java
@@ -1,19 +1,35 @@
 package com.maku.zawadi;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.Task;
 import com.hbb20.CountryCodePicker;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public  static  final String TAG = LoginActivity.class.getSimpleName();
+    private GoogleApiClient mGoogleApiClient;
 
     @BindView(R.id.loginButton) Button mLoginButton;
     @BindView(R.id.wolcomeTextView)
@@ -25,6 +41,9 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     @BindView(R.id.ccp)
     CountryCodePicker ccp;
 
+    @BindView(R.id.sign_in_button)
+     SignInButton googleSignInButton;
+    private GoogleSignInClient googleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +52,27 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         ButterKnife.bind(this);
 
-        mLoginButton.setOnClickListener(this);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestId()
+                .requestEmail()
+                .build();
+        googleSignInClient = GoogleSignIn.getClient(this, gso);
 
+        mLoginButton.setOnClickListener(this);
+        googleSignInButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        GoogleSignInAccount alreadyloggedAccount = GoogleSignIn.getLastSignedInAccount(this);
+        if (alreadyloggedAccount != null) {
+            Toast.makeText(this, "Already Logged In", Toast.LENGTH_SHORT).show();
+            onLoggedIn(alreadyloggedAccount);
+        } else {
+            Log.d(TAG, "Not logged in");
+        }
     }
 
     @Override
@@ -58,7 +96,41 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 }
             });
 
+        } else if(v == googleSignInButton){
+            Intent signInIntent = googleSignInClient.getSignInIntent();
+            startActivityForResult(signInIntent, 101);
         }
 
+    }
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == Activity.RESULT_OK)
+            switch (requestCode) {
+                case 101:
+                    try {
+                        // The Task returned from this call is always completed, no need to attach
+                        // a listener.
+                        Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+                        GoogleSignInAccount account = task.getResult(ApiException.class);
+                        Log.d(TAG, account.getEmail());
+                        onLoggedIn(account);
+                    } catch (ApiException e) {
+                        // The ApiException status code indicates the detailed failure reason.
+                        Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+                    }
+                    break;
+            }
+    }
+
+
+    private void onLoggedIn(GoogleSignInAccount account) {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        intent.putExtra(MainActivity.GOOGLE_ACCOUNT, account);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/maku/zawadi/MainActivity.java
+++ app/src/main/java/com/maku/zawadi/MainActivity.java
@@ -2,13 +2,21 @@ package com.maku.zawadi;
 
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.hbb20.CountryCodePicker;
+import com.squareup.picasso.Picasso;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -20,6 +28,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @BindView(R.id.appName)TextView mAppNameTextView;
     @BindView(R.id.restaurant)
     CardView mRestaurant;
+
+    public static final String GOOGLE_ACCOUNT = "google_account";
+     @BindView(R.id.profile_text) TextView profileName ;
+    @BindView(R.id.profile_email) TextView profileEmail;
+    @BindView(R.id.profile_image) ImageView profileImage;
+    @BindView(R.id.sign_out) Button signOut;
+
+    private GoogleSignInClient googleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +54,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mmobile_numberTextView.setText(mobile);
 
         mRestaurant.setOnClickListener(this);
+
+        GoogleSignInAccount googleSignInAccount = getIntent().getParcelableExtra(GOOGLE_ACCOUNT);
+        Picasso.get().load(googleSignInAccount.getPhotoUrl().toString()).centerInside().fit().into(profileImage);
+        profileName.setText(googleSignInAccount.getDisplayName());
+        profileEmail.setText(googleSignInAccount.getEmail());
+
+    }
+
+    private void setDataOnView() {
+
     }
 
     @Override
@@ -47,6 +73,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
             startActivity(intent);
+        } else if(v == signOut){
+
+             /*
+          Sign-out is initiated by simply calling the googleSignInClient.signOut API. We add a
+          listener which will be invoked once the sign out is the successful
+           */
+            googleSignInClient.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    //On Succesfull signout we navigate the user back to LoginActivity
+                    Intent intent=new Intent(MainActivity.this,LoginActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                }
+            });
+
         }
 
     }
