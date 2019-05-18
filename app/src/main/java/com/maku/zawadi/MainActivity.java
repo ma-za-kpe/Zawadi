aaa157f0d5996b8e16650c59013400972e35c6ba
ma-za-kpe <makpalyy@gmail.com>
2019-05-18 20:38:25 +0200

    added icons into the navigation drawer menu items

--- app/src/main/java/com/maku/zawadi/MainActivity.java
+++ app/src/main/java/com/maku/zawadi/MainActivity.java
@@ -1,15 +1,20 @@
 package com.maku.zawadi;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.graphics.Typeface;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -47,7 +52,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     public  static  final String TAG = MainActivity.class.getSimpleName();
 
-    @BindView(R.id.appName)TextView mAppNameTextView;
     @BindView(R.id.restaurant)
     CardView mRestaurant;
     private FirebaseAuth mAuth;
@@ -60,10 +64,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @BindView(R.id.profile_image) ImageView profileImage;
     @BindView(R.id.sign_out) Button signOut;
 
+    private ActionBarDrawerToggle mDrawerToggle;
+    private String mActivityTitle;
 
-    private GoogleSignInClient googleSignInClient;
-    //It is the verification id that will be sent to the user
-    private String mVerificationId;
+    @BindView(R.id.drawer_layout) DrawerLayout drawerLayout;
+    @BindView(R.id.toolbar_main) Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,8 +78,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         ButterKnife.bind(this);
 
+        setSupportActionBar(toolbar);
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close){
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle("Navigation!");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle(mActivityTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+        drawerLayout.addDrawerListener(mDrawerToggle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
         Typeface kushanScriptRegular = Typeface.createFromAsset(getAssets(), "fonts/kaushanscriptregular.otf");
-        mAppNameTextView.setTypeface(kushanScriptRegular);
 
         Intent intent = getIntent();
         String email = intent.getStringExtra("email");
@@ -179,4 +206,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
 }
