6632e59667a7748c90913e7a3fc19f0c7bc9346a
ma-za-kpe <makpalyy@gmail.com>
2019-05-22 14:42:11 +0200

    added override methods in the GetNearByPlaces.java class

--- app/src/main/java/com/maku/zawadi/googlePLaces/GetNearByPlacesData.java
+++ app/src/main/java/com/maku/zawadi/googlePLaces/GetNearByPlacesData.java
@@ -1,4 +1,29 @@
 package com.maku.zawadi.googlePLaces;
 
-public class GetNearByPlacesData {
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.maku.zawadi.MainActivity;
+
+public class GetNearByPlacesData extends AsyncTask<Object, String, String> {
+
+    public  static  final String TAG = GetNearByPlacesData.class.getSimpleName();
+
+    //vars
+    String googlePlacesData;
+    GoogleMap googleMap;
+    String url;
+
+    @Override
+    protected void onPostExecute(String s) {
+        Log.d(TAG, "onPostExecute: onPostExecute");
+        super.onPostExecute(s);
+    }
+
+    @Override
+    protected String doInBackground(Object... objects) {
+        Log.d(TAG, "doInBackground: do in background");
+        return null;
+    }
 }
