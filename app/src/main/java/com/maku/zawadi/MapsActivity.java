287cfaaaf0a64ba3dd7f922fbaacb9e7e5f4ebd3
ma-za-kpe <makpalyy@gmail.com>
2019-05-22 23:10:34 +0200

    added the get nearby restaurants functionality

--- app/src/main/java/com/maku/zawadi/MapsActivity.java
+++ app/src/main/java/com/maku/zawadi/MapsActivity.java
@@ -4,30 +4,52 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.maku.zawadi.model.Example;
+import com.maku.zawadi.model.NearByApiResponse;
+import com.maku.zawadi.networking.NearByApi;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
     public static final String TAG = "MapsActivity";
 
+    private double currentLatitude; //lat of user
+    private double currentLongitude; //long of user
+
     private GoogleMap mMap;
+    private GoogleApiClient mGoogleApiClient;
     private FusedLocationProviderClient mfusedLocationProviderClient;
 
 
@@ -41,14 +63,39 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     //variables
     private Boolean mLocationPermissionGranted = false;
 
+    //places
+    private GoogleMap googleMap;
+    private Button btnRestorentFind, btnHospitalFind;
+    private LocationRequest mLocationRequest;
+    private Location location;
+    private int PROXIMITY_RADIUS = 8000;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
+        btnRestorentFind = (Button) findViewById(R.id.rest);
         //location permissions
         getLocationPermission();
+    }
+
+    //contains your lat and lon
+    private void handleNewLocation(Location mlocation) {
+        Log.d(TAG, mlocation.toString());
+
+        currentLatitude = mlocation.getLatitude();
+        currentLongitude = mlocation.getLongitude();
+
+        LatLng latLng = new LatLng(currentLatitude, currentLongitude);
+        Log.d(TAG, String.valueOf(latLng));
+
+        MarkerOptions options = new MarkerOptions()
+                .position(latLng)
+                .title("You are here");
+        mMap.addMarker(options);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom((latLng), 11.0F));
     }
 
 
@@ -147,12 +194,105 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * This is where we can a protected synchronized void buildGoogleApiClient() {
+     mGoogleApiClient = new GoogleApiClient.Builder(this).addConnectionCallbacks(this).addOnConnectionFailedListener(this).addApi(LocationServices.API).build();
+     mGoogleApiClient.connect();
+     }
+
+     public void onRestorentFindClick(View view){
+     findPlaces("restaurant");
+     }
+     dd markers or lines, add listeners or move the camera. In this case,
      * we just add a marker near Sydney, Australia.
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
+    protected synchronized void buildGoogleApiClient() {
+        Log.d(TAG, "buildGoogleApiClient: ");
+        mGoogleApiClient = new GoogleApiClient.Builder(this).addConnectionCallbacks(this).addOnConnectionFailedListener(this).addApi(LocationServices.API).build();
+        mGoogleApiClient.connect();
+    }
+
+    public void findPlaces(String placeType) {
+        Log.d(TAG, "findPlaces: place..." + placeType);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+        Log.d(TAG, "findPlaces: ..." + mLastLocation);
+
+        try{
+
+
+            String url = "https://maps.googleapis.com/maps/";
+
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl(url)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+
+            Log.d(TAG, "findPlaces: URL..." + retrofit.toString());
+
+            NearByApi service = retrofit.create(NearByApi.class);
+
+
+            Call<Example> call = service.getNearbyPlaces(placeType, mLastLocation.getLatitude() + "," + mLastLocation.getLongitude(), PROXIMITY_RADIUS);
+
+            call.enqueue(new Callback<Example>() {
+                @Override
+                public void onResponse(Call<Example> call, Response<Example> response) {
+                    try {
+                        mMap.clear();
+                        // This loop will go through all the results and add marker on each location.
+                        for (int i = 0; i < response.body().getResults().size(); i++) {
+                            Double lat = response.body().getResults().get(i).getGeometry().getLocation().getLat();
+                            Double lng = response.body().getResults().get(i).getGeometry().getLocation().getLng();
+                            String placeName = response.body().getResults().get(i).getName();
+                            String vicinity = response.body().getResults().get(i).getVicinity();
+
+                            Log.d(TAG, "findPlaces: URL..." + placeName);
+                            Log.d(TAG, "findPlaces: URL..." + vicinity);
+
+                            MarkerOptions markerOptions = new MarkerOptions();
+                            LatLng latLng = new LatLng(lat, lng);
+                            // Location of Marker on Map
+                            markerOptions.position(latLng);
+                            // Title for Marker
+                            markerOptions.title(placeName + " : " + vicinity);
+                            // Color or drawable for marker
+                            markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+                            // add marker
+                            Marker m = mMap.addMarker(markerOptions);
+                            // move map camera
+                            mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                            mMap.animateCamera(CameraUpdateFactory.zoomTo(13));
+                        }
+                    } catch (Exception e) {
+                        Log.d("onResponse", "There is an error");
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<Example> call, Throwable t) {
+                    Log.d("onFailure", t.toString());
+                    t.printStackTrace();
+                    PROXIMITY_RADIUS += 10000;
+                }
+            });
+
+        }
+        catch (Exception e)
+        {
+            Log.d(TAG, "findPlaces: " + e.getMessage());
+        }
+
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         Toast.makeText(this, "map is ready", Toast.LENGTH_LONG).show();
@@ -166,12 +306,64 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                 return;
             }
+            buildGoogleApiClient();
             mMap.setMyLocationEnabled(true);
             mMap.getUiSettings().setMyLocationButtonEnabled(true);
 
         }
+
+        btnRestorentFind.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findPlaces("restaurant");
+            }
+        });
 //        LatLng nairobi = new LatLng(-1.28333, 36.81667);
 //        mMap.addMarker(new MarkerOptions().position(nairobi).title("Marker in Nairobi"));
 //        mMap.moveCamera(CameraUpdateFactory.newLatLng(nairobi));
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.d(TAG, "onConnected: onConnected...");
+        startLocationUpdates();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d(TAG, "onConnectionFailed: Could not connect google api");
+        Toast.makeText(this,"Could not connect google api",Toast.LENGTH_LONG).show();
+    }
+
+    protected void startLocationUpdates() {
+        Log.d(TAG, "startLocationUpdates: startLocationUpdates");
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(1000);
+        mLocationRequest.setFastestInterval(1000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.d(TAG, "onLocationChanged: onLocationChanged");
+        if(location!=null){
+            this.location = location;
+            if(!btnHospitalFind.isEnabled()){
+                LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());
+                googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                googleMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+
+                btnRestorentFind.setEnabled(true);
+                btnHospitalFind.setEnabled(true);
+            }
+        }
     }
 }
--- app/src/main/java/com/maku/zawadi/networking/NearByApi.java
+++ app/src/main/java/com/maku/zawadi/networking/NearByApi.java
@@ -1,6 +1,6 @@
 package com.maku.zawadi.networking;
 
-import com.maku.zawadi.model.NearByApiResponse;
+import com.maku.zawadi.model.Example;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
@@ -8,10 +8,12 @@ import retrofit2.http.Query;
 
 public interface NearByApi {
 
-    @GET("api/place/nearbysearch/json?sensor=true&key=AIzaSyBxn7Mc7qHoABw-HePZYUFTklINIGFKtis") //used to call server corresponding to url
-    Call<NearByApiResponse> getNearbyPlaces(@Query("type") String type,
-                                            @Query("location") String location,
-                                            @Query("radius") int radius);
+    @GET("api/place/nearbysearch/json?sensor=true&key=AIzaSyBxn7Mc7qHoABw-HePZYUFTklINIGFKtis")
+    Call<Example> getNearbyPlaces(@Query("type") String type, @Query("location") String location, @Query("radius") int radius);
+
+
+
+    // https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-1.28333,36.81667&radius=1500&type=supermarket&key=AIzaSyBxn7Mc7qHoABw-HePZYUFTklINIGFKtis
 }
 
 
